96dd977750938b33b649738315e3ee69dec391ae
ISC-VANBT <ISC-VANBT>
2017-05-03 09:17:54 +0700

    update

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 var userModel = new Schema({
     name: {type: String, required: [true, 'Họ tên không được để trống']},
     username: {type: String, required: [true, 'Tên đăng nhập không được để trống']},
-    password: {type: String, required: [true, 'Mật khẩu không được để trống']},,
+    password: {type: String, required: [true, 'Mật khẩu không được để trống']},
 	role: {type: Schema.Types.ObjectId, ref: 'Role'},
     isdelete: {type: Boolean, default: true}
 },
